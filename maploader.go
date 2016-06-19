d6f86c49c2efdd50c6292e3b399f89f96a9019e0
Igor Pavlov <gophan1992@gmail.com>
2016-06-19 10:34:48 +0300

    temporary

--- maploader.go
+++ maploader.go
@@ -16,15 +16,11 @@ type MapLoader struct {
 
 
 type PlanetOsmPolygon struct {
-	OsmId 		string
-	Amenity 	string
-	Building 	string
-	Name 		string
 	WayJson 	string
-	Distance	float64
 }
 
-func (m MapLoader) FindNearest(x float64, y float64) []byte {
+
+func (m MapLoader) FindNearest(x float64, y float64) string {
 
 	m.Db = pg.Connect(&pg.Options{
 		User: 		m.Username,
@@ -36,11 +32,7 @@ func (m MapLoader) FindNearest(x float64, y float64) []byte {
 	var buildings []PlanetOsmPolygon
 
 	var query = fmt.Sprintf(`
-	SELECT osm_id,
-	       amenity,
-	       name,
-	       way_json::json as way_json,
-	       ST_Distance(column1::geography, centroid::geography) as distance
+	SELECT way_json::json->'coordinates'->0 as way_json
 	FROM planet_osm_polygons,
 	     (
 	      VALUES(ST_PointFromText('Point( %.8f %.8f)', 4326)) ) AS new_value
@@ -48,18 +40,21 @@ func (m MapLoader) FindNearest(x float64, y float64) []byte {
 	  AND area IS NULL
 	  AND building IS NOT NULL
 	  AND ST_Distance(ST_Transform(column1, 26986), ST_Transform(centroid, 26986)) < 1000
-	ORDER BY ST_Distance(column1::geography, centroid::geography) LIMIT 2;`, x, y)
+	ORDER BY ST_Distance(column1::geography, centroid::geography) LIMIT 20;`, x, y)
 	print(query)
 	_, err := m.Db.Query(&buildings, query)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(len(buildings))
-	fmt.Println(buildings[0].OsmId, buildings[0].Amenity, buildings[0].Building, buildings[0].Name, buildings[0].WayJson)
-	jsonned, err := json.Marshal(buildings)
+	var result = []string{}
+	for _, building := range buildings {
+		result = append(result, building.WayJson)
+	}
+
+	jsonned, err := json.Marshal(result)
 	if err != nil {
 		panic(err)
 	}
-	return jsonned
+	return string(jsonned)
 }
